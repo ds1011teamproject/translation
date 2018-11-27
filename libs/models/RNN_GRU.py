64fdde81b9c6d0055c8c02c48c7c804f0476a305
Rong Feng <rmfeng@gmail.com>
2018-11-26 20:22:51 -0500

    no grad on beam

--- libs/models/RNN_GRU.py
+++ libs/models/RNN_GRU.py
@@ -48,9 +48,10 @@ class RNN_GRU(MTBaseModel):
         # decoding - beamsearch
         if mode == DecodeMode.TRANSLATE_BEAM:
             # implement beam search here
-            predicted = beam_search(dec_in, enc_results, self.decoder, tgt_batch.size(1),
-                                    self.hparams[HyperParamKey.BEAM_SEARCH_WIDTH])
-            return predicted
+            with torch.no_grad():
+                predicted = beam_search(dec_in, enc_results, self.decoder, tgt_batch.size(1),
+                                        self.hparams[HyperParamKey.BEAM_SEARCH_WIDTH])
+                return predicted
         else:
             # decoding - regular
             for t in range(tgt_batch.size(1)):
